2bb26718ed02746e08db43045daaf9171e0c1670
yoannd <yoanndewilde@gmail.com>
2020-06-25 20:58:04 +0200

    Separate instructions and expressions (Node enum). Implement eval function

--- src/ast.rs
+++ src/ast.rs
@@ -1,5 +1,25 @@
+pub enum Instruction {
+    Print(Node),
+}
+
 pub enum Node {
     Int(i32),
     Add(Box<Node>, Box<Node>),
-    Print(Box<Node>),
+}
+
+impl Node {
+    pub fn eval(&self) -> i32 {
+        match self {
+            Node::Int(i) => i,
+            Node::Add(n1, n2) => n1.eval() + n2.eval(),
+        }
+    }
+}
+
+impl Instruction {
+    pub fn eval(&self) {
+        match self {
+            Instruction::Print(n) => println!("{}", n.eval()),
+        }
+    }
 }
