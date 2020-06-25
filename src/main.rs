5d856ffeae63068148026a247923a2a848c0697e
yoannd <yoanndewilde@gmail.com>
2020-06-25 21:02:10 +0200

    Test with "5 + 3" => 8

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 mod ast;
 
+use ast::*;
+
 fn main() {
-    println!("Hello, world!");
+    // print(5 + 3)
+    let code = Instruction::Print(Node::Add(Box::new(Node::Int(5)), Box::new(Node::Int(3))));
+
+    code.eval();
 }
