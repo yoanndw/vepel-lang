b2bfdb7f6331d1fce2c9b5775973eb982dc54d29
yoannd <yoanndewilde@gmail.com>
2020-06-26 20:10:33 +0200

    [AST] Add subtraction, multiplication, and division

--- src/ast.rs
+++ src/ast.rs
@@ -5,6 +5,9 @@ pub enum Instruction {
 pub enum Node {
     Int(i32),
     Add(Box<Node>, Box<Node>),
+    Sub(Box<Node>, Box<Node>),
+    Mul(Box<Node>, Box<Node>),
+    Div(Box<Node>, Box<Node>),
 }
 
 impl Node {
@@ -12,6 +15,9 @@ impl Node {
         match self {
             Node::Int(i) => *i,
             Node::Add(n1, n2) => n1.eval() + n2.eval(),
+            Node::Sub(n1, n2) => n1.eval() - n2.eval(),
+            Node::Mul(n1, n2) => n1.eval() * n2.eval(),
+            Node::Div(n1, n2) => n1.eval() / n2.eval(),
         }
     }
 }
